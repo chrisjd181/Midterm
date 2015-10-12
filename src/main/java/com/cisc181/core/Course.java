848712315fbe6c907ee4ee5dcb4c21a614979ac8
Chris <chrisjd@udel.edu>
2015-10-12 16:41:30 -0400

    changes

--- src/main/java/com/cisc181/core/Course.java
+++ src/main/java/com/cisc181/core/Course.java
@@ -1,5 +1,32 @@
 package com.cisc181.core;
 
-public class Course {
+import java.util.UUID;
+
+public class Course{
+
+		private UUID CourseID;
+		private String CourseName;
+		private int GradePoints;
+		public UUID getCourseID() {
+			return CourseID;
+		}
+		public void setCourseID(UUID courseID) {
+			CourseID = courseID;
+		}
+		public String getCourseName() {
+			return CourseName;
+		}
+		public void setCourseName(String courseName) {
+			CourseName = courseName;
+		}
+		public int getGradePoints() {
+			return GradePoints;
+		}
+		public void setGradePoints(int gradePoints) {
+			GradePoints = gradePoints;
+		}
+
+
+
 
 }
